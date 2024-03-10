eee1adc2ad62a75ca1e76ff23200ae54f63f199e
Yufei Shi <yfshi@cmu.edu>
2024-03-10 01:26:19 -0500

    [cpu/decoder]: Add an initial decoder declaration

---
+++ src/cpu/decoder.rs
@@ -0,0 +1,19 @@
+
+
+use crate::cpu::defs::*;
+
+/**
+ *
+ */
+pub fn decode(instruction: u32) -> Option<Instr> {
+    None
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_todo() {
+        assert!(true);
+    }
+}
